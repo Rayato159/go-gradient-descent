314a01961f64d38c86728983345a05755186a754
Rayato159 <rayato159@gmail.com>
2022-12-18 00:40:37 +0700

    -feature :sparkles: :python services have been added

--- pkg/plots/main.py
+++ pkg/plots/main.py
@@ -1,9 +1,13 @@
-import numpy as np
+import os
 import random
 import time
+
+from flask import Flask, send_file
 from pymongo import MongoClient
+from matplotlib import pyplot as plt
+import numpy as np
+
 from dotenv import load_dotenv
-import os
 
 class Database:
     def __init__(self):
@@ -23,8 +27,13 @@ class Database:
         # Create the database for our example (we will use the same database throughout the tutorial
         return db[f"{self.database}"]
 
-def main(db: MongoClient):
+# Load database
+load_dotenv("../../.env.dev")
+db = Database().get_db()
+
+def rand_data():
     from sklearn.model_selection import train_test_split
+    random.seed(time.time_ns())
     
     x = np.linspace(start=-10, stop=10, num=100)
 
@@ -58,23 +67,45 @@ def main(db: MongoClient):
 
     print("success, all data has been inserted")
 
-    from matplotlib import pyplot as plt
-    
+def get_plot():
+    latest_record = db["records"].find_one(sort=[("$natural", -1)])
+    test_data = db["test_data"].find()
+
+    params, err = latest_record["weights"], latest_record["error_test"]
+
+    x_test, y_test, y_true = [], [], []
+    for i in test_data:
+        x_test.append(float(i["x"]))
+        y_test.append(float(i["y"]))
+
+    slope, y_intercept = float(params[0]), float(params[1])
+    for i in x_test:
+        y_true.append(params[0]*i + params[1])
+
     op = "+" if y_intercept > 0 else ""
     op = "+" if y_intercept > 0 else ""
     plt.figure(figsize=(5,4))
     plt.title(f"f(x) = {slope:.6f}x {op} {y_intercept:.6f}")
     plt.xlabel("x")
     plt.ylabel("y")
-    plt.scatter(x, y, color="#FF597B")
-    plt.plot(x, y_true, color="#2B3A55")
-    plt.show()
+    plt.scatter(x_test, y_test, color="#FF597B")
+    plt.plot(x_test, y_true, color="#2B3A55")
+    plt.savefig('test_1.png')
+
+# Service API
+app = Flask(__name__)
+
+@app.route('/insert-rand-data', methods=["POST"])
+def insert_rand_data():
+    # rand_data()
+    return {
+        "message": "success, data has been inserted"
+    }, 201
+
+@app.route('/get-plot', methods=["GET"])
+def stream_file():
+    get_plot()
+    return send_file("./test_1.png", mimetype="image/png"), 200
 
 if __name__ == "__main__":
-    random.seed(time.time_ns())
-    load_dotenv("../../.env.dev")
-
-    # Load configs
-    db = Database()
-
-    main(db)
+    app.run()
